016106a4ffd583e691383c6425a4daaa6d120666
xSid2005x <108790009+xSid2005x@users.noreply.github.com>
2024-08-13 17:22:57 +0800

    Update processing.py

--- src/Processing/processing.py
+++ src/Processing/processing.py
@@ -93,7 +93,4 @@ def predict():
 
 if __name__ == "__main__":
     logging.info("Starting Flask server")
-    app.run(host='0.0.0.0', port=5003, debug=True)
-
-
-
+    app.run(host='0.0.0.0', port=5003)
